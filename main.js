8b2c1eb031af359fd2ee794a01cb5336a6a34bd0
jpablux <mail@jpabloestrada.com>
2022-11-07 22:30:11 -0600

    adding first javascript interactions

---
+++ main.js
@@ -0,0 +1,56 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu ');
+const burguerMenu = document.querySelector('.burguerMenu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartMenu);
+
+
+function toggleDesktopMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+   desktopMenu.classList.toggle('inactive'); 
+
+}
+
+function toggleMobileMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive');
+   
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
+   
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+function toggleCartMenu(){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+   
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
+
+}
